3e86389828a72b16aa5bc7221c9d83e54c879b51
Ashton Jones <ashtonjonesdev@gmail.com>
2020-09-11 17:18:19 -0400

    Removed all Firebase code and dependencies
    Removed all unused/unnecessary screens

---
+++ lib/ui/screens/entry_point.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:stream_chat_with_firebase/core/utils/shared_prefs.dart';
+import 'package:stream_chat_with_firebase/styles/colors.dart';
+import 'package:stream_chat_with_firebase/ui/widgets/custom_button.dart';
+
+import 'home.dart';
+
+class EntryPoint extends StatelessWidget {
+  final TextEditingController _textEditingController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('SpikeChat',
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2
+                .copyWith(color: kAccentColor)),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            'images/app_icon.png',
+            width: 150,
+            height: 150,
+          ),
+          Form(
+            key: _formKey,
+            child: TextFormField(
+              controller: _textEditingController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter the passcode...',
+              ),
+              validator: (input) {
+                if (input.isEmpty) {
+                  return 'Enter some text';
+                }
+                if (input == 'spiketally') {
+                  return null;
+                }
+                return 'You do not have access to this group';
+              },
+            ),
+          ),
+          CustomButton(
+            onPressed: () async {
+              if (_formKey.currentState.validate()) {
+                sharedPrefs.isValidated = true;
+
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (_) => Home()));
+              }
+            },
+            buttonText: 'VERIFY',
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/ui/screens/home.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:stream_chat/stream_chat.dart';
+import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+import 'package:stream_chat_with_firebase/core/data/model/chat_model.dart';
+import 'package:stream_chat_with_firebase/core/utils/shared_prefs.dart';
+import 'package:stream_chat_with_firebase/styles/colors.dart';
+import 'package:stream_chat_with_firebase/styles/theme.dart';
+import 'package:stream_chat_with_firebase/ui/widgets/custom_button.dart';
+import 'package:stream_chat_with_firebase/ui/widgets/custom_form.dart';
+import 'package:stream_chat_with_firebase/ui/widgets/stream_chat_widgets.dart';
+
+class Home extends StatelessWidget {
+
+  static final String routeName = 'home';
+
+
+  final TextEditingController _textEditingController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final chatModelProvider = Provider.of<ChatModel>(context);
+
+    return sharedPrefs.username == null
+        ? Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('SpikeChat',
+            style: Theme.of(context)
+                .textTheme
+                .bodyText2
+                .copyWith(color: kAccentColor, fontSize: 24)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/app_icon.png',
+              width: 150,
+              height: 150,
+            ),
+            CustomForm(
+              textEditingController: _textEditingController,
+              formKey: _formKey,
+              hintText: 'Enter a username...',
+            ),
+            CustomButton(
+              onPressed: () async {
+                if (_formKey.currentState.validate()) {
+                  final userId = _textEditingController.value.text;
+                  final client = chatModelProvider.streamClient;
+
+                  sharedPrefs.username = userId;
+
+                  await client.setUserWithProvider(User(
+                      id: 'id_$userId',
+                      extraData: {
+                        'name': '$userId',
+                        'image': 'https://picsum.photos/100/100'
+                      }));
+
+                  final channel = client
+                      .channel('mobile', id: 'Spikeball', extraData: {
+                    'image':
+                    'https://scheels.scene7.com/is/image/Scheels/85375900555?wid=400&hei=400&qlt=50'
+                  });
+                  channel.watch();
+
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (_) => StreamChat(
+                        streamChatThemeData: StreamChatThemeData
+                            .fromTheme(AppTheme.appThemeData.copyWith(
+                            textTheme:
+                            GoogleFonts.robotoTextTheme())),
+                        client: client,
+                        child: StreamChannel(
+                          channel: channel,
+                          child: ChannelPage(),
+                        ),
+                      )));
+                }
+              },
+              buttonText: 'SUBMIT',
+            )
+          ],
+        ),
+      ),
+    )
+        : Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('SpikeChat',
+            style: Theme.of(context)
+                .textTheme
+                .headline3
+                .copyWith(color: kAccentColor, fontSize: 24)),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              'images/app_icon.png',
+              width: 150,
+              height: 150,
+            ),
+            Material(
+              child: Text(
+                '${sharedPrefs.username}',
+                style: Theme.of(context)
+                    .textTheme
+                    .bodyText1
+                    .copyWith(fontSize: 20),
+              ),
+            ),
+            CustomButton(
+              onPressed: () async {
+
+                print('Clicked continue!');
+
+                final client = chatModelProvider.streamClient;
+
+                await client.setUserWithProvider(User(
+                    id: 'id_${sharedPrefs.username}',
+                    extraData: {
+                      'name': '${sharedPrefs.username}',
+                      'image': 'https://picsum.photos/100/100'
+                    }));
+
+                final channel =
+                client.channel('mobile', id: 'Spikeball', extraData: {
+                  'image':
+                  'https://scheels.scene7.com/is/image/Scheels/85375900555?wid=400&hei=400&qlt=50'
+                });
+                channel.watch();
+
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) => StreamChat(
+                      streamChatThemeData:
+                      StreamChatThemeData.fromTheme(
+                          AppTheme.appThemeData.copyWith(
+                              textTheme:
+                              GoogleFonts.robotoTextTheme())),
+                      client: client,
+                      child: StreamChannel(
+                        channel: channel,
+                        child: ChannelPage(),
+                      ),
+                    )));
+              },
+              buttonText: 'CONTINUE',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
