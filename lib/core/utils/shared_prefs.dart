aa6f5629511ef94965e64abb3facaf90129b89fd
Ashton Jones <ashtonjonesdev@gmail.com>
2020-09-12 13:46:31 -0400

    Formatted Dart code

--- lib/core/utils/shared_prefs.dart
+++ lib/core/utils/shared_prefs.dart
@@ -2,37 +2,25 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:spike_chat/styles/constants.dart';
 
 class SharedPrefs {
-
   static SharedPreferences _sharedPreferences;
 
   init() async {
-
-    if(_sharedPreferences == null) {
-
+    if (_sharedPreferences == null) {
       _sharedPreferences = await SharedPreferences.getInstance();
-
     }
-
   }
 
   bool get isValidated => _sharedPreferences.getBool(IS_VALIDATED_KEY) ?? false;
 
   set isValidated(bool validation) {
-
     _sharedPreferences.setBool(IS_VALIDATED_KEY, validation);
-
   }
 
   String get username => _sharedPreferences.getString(USERNAME_KEY) ?? null;
 
   set username(String username) {
-
     _sharedPreferences.setString(USERNAME_KEY, username);
-
   }
-
-
-
 }
 
 final sharedPrefs = SharedPrefs();
--- lib/styles/theme.dart
+++ lib/styles/theme.dart
@@ -4,7 +4,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'colors.dart';
 
 class AppTheme {
-
   static ThemeData appThemeData = ThemeData.light().copyWith(
     visualDensity: VisualDensity.adaptivePlatformDensity,
     primaryColor: kPrimaryColor,
@@ -14,5 +13,4 @@ class AppTheme {
     errorColor: kErrorColor,
     textTheme: GoogleFonts.robotoTextTheme(),
   );
-
 }
